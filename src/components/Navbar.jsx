feae277503348b05322e4172704fe2c193193980
sam_wlh_ds <sam.wlh.ds@gmail.com>
2024-10-20 13:11:59 +0530

    More CSS

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,52 +1,72 @@
-import React, { useState } from 'react'
-import {useNavigate} from "react-router-dom";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
   const [openSidebar, setOpenSideBar] = useState(false);
 
   const handleSidebar = () => {
     setOpenSideBar(!openSidebar);
-  }
+  };
 
   const navigate = useNavigate();
 
-  const dashboard = ()=>{
+  const dashboard = () => {
     navigate("/dashboard");
-  }
+  };
 
-  const visual = ()=>{
+  const visual = () => {
     navigate("/visualization");
-  }
+  };
 
-  const cam = ()=>{
+  const cam = () => {
     navigate("/cam");
-  }
+  };
 
-  const home = ()=>{
+  const home = () => {
     navigate("/");
-  }
+  };
   return (
-
     <main>
-
       <nav>
         <ul>
-          <li><a href='#'>Smart Home</a></li>
-          <button onClick={handleSidebar} type="button" className="btn">&#8801;</button>
+          <li>
+            <a
+              href="#"
+              className="fs-2"
+              style={{ fontFamily: "Trirong, sans-serif" }}
+            >
+              <strong>GRHA</strong>
+            </a>
+          </li>
+          <button onClick={handleSidebar} type="button" className="btn fs-2 mx-1">
+            &#8801;
+          </button>
         </ul>
         {openSidebar && (
-          <ul className='sidebar'>
-            <button onClick={handleSidebar} type="button" class="btn-close" aria-label="Close"></button>
-            <li><a onClick={home}>Home</a></li>
-            <li><a onClick={dashboard}>Dashboard</a></li>
-            <li><a onClick={visual}>Energy Consumption</a></li>
-            <li><a onClick={cam}>Security Monitoring</a></li>
+          <ul className="sidebar">
+            <button
+              onClick={handleSidebar}
+              type="button"
+              class="btn-close m-3"
+              aria-label="Close"
+            ></button>
+            <li>
+              <a onClick={home}>Home</a>
+            </li>
+            <li>
+              <a onClick={dashboard}>Dashboard</a>
+            </li>
+            <li>
+              <a onClick={visual}>Energy Consumption</a>
+            </li>
+            <li>
+              <a onClick={cam}>Security Monitoring</a>
+            </li>
           </ul>
         )}
       </nav>
-
     </main>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
